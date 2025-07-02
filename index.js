a36e70c265eb7719b487140b8ee5f4ebb8821580
f1cus7 <f1cus7@mail.ru>
2025-07-02 03:41:03 +0300

    0.3 вкладки "настройка" и фундамент для "характеристик"

--- index.js
+++ index.js
@@ -1,10 +1,64 @@
+const main = document.querySelector('.main-container')
+const transport = document.querySelector('.transport-container')
+const stamina = document.querySelector('.stamina-container')
+const happy = document.querySelector('.happy-container')
+const update = document.querySelector('.update-container')
 
-document.getElementById('btn-transport').addEventListener('click', () => {
-    document.querySelector('.main-container').classList.add('d-none')
-    document.querySelector('.transport-container').classList.remove('d-none')
-})
+const openTransport = () => {
+    main.classList.add('d-none')
+    stamina.classList.add('d-none')
+    happy.classList.add('d-none')
+    update.classList.add('d-none')
+    transport.classList.remove('d-none')
+}
+
 
 const backToMain = () => {
-    document.querySelector('.main-container').classList.remove('d-none')
-    document.querySelector('.transport-container').classList.add('d-none')
+    main.classList.remove('d-none')
+    stamina.classList.add('d-none')
+    happy.classList.add('d-none')
+    update.classList.add('d-none')
+    transport.classList.add('d-none')
 }
+
+const btnStamina = () => {
+    main.classList.add('d-none')
+    stamina.classList.remove('d-none')
+    happy.classList.add('d-none')
+    update.classList.add('d-none')
+    transport.classList.add('d-none')
+}
+
+const btnHappy = () => {
+    main.classList.add('d-none')
+    stamina.classList.add('d-none')
+    happy.classList.remove('d-none')
+    update.classList.add('d-none')
+    transport.classList.add('d-none')
+}
+
+const btnUpdate = () => {
+    main.classList.add('d-none')
+    stamina.classList.add('d-none')
+    happy.classList.add('d-none')
+    update.classList.remove('d-none')
+    transport.classList.add('d-none')
+}
+
+const modal = document.getElementById("myModal");
+const openBtn = document.getElementById("openModalBtn");
+const closeBtn = document.querySelector(".modal-settings-close");
+
+const openSettings = () => {
+  modal.style.display = "flex";
+};
+
+closeBtn.onclick = () => {
+  modal.style.display = "none";
+};
+
+window.onclick = (event) => {
+  if (event.target === modal) {
+    modal.style.display = "none";
+  }
+};
